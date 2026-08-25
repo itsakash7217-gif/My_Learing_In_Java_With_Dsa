72c3a09ffceeee5fb2d3f140e6c1f8cfea1b1a4a
Akash Kushwaha <244578967+itsakash7217-gif@users.noreply.github.com>
2026-08-26 00:08:32 +0530

    Generic arraylist

--- src/oopsClass/ArraysLists/arraylist.java
+++ src/oopsClass/ArraysLists/arraylist.java
@@ -1,38 +1,38 @@
 package oopsClass.ArraysLists;
 import java.util.Arrays;
 import java.util.Scanner;
-public class arraylist {
-    private int[] arr;
+public class arraylist<T> {
+    private Object[] arr;
     private static int Default = 10;
     private int size= 0;
 
 
     arraylist(){
-        this.arr = new int[Default];
+        this.arr = new Object[Default];
     }
-    void add(int n){
+    void add(Object n){
         if(isfull()){
             newone();
         }
         arr[size++]=n;
     }
     void newone(){
-        int[] temp = new int[arr.length*2];
+        Object[] temp = new Object[arr.length*2];
         for(int i = 0 ; i < size ; i++){
             temp[i]=arr[i];
         }
         arr=temp;
     }
-    int remove(){
-        int removed=arr[size-1];
+    Object remove(){
+        Object removed=arr[size-1];
         size--;
         return removed;
     }
-    int get(){
+    Object get(){
 
         return arr[size-1];
     }
-    void set(int n,int m){
+    void set(int n,Object m){
         if(isfull()){
             newone();
         }
@@ -47,7 +47,7 @@ public class arraylist {
     public int Size(){
         return size;
     }
-    int get(int i){
+    Object get(int i){
         return arr[i];
     }
 
@@ -57,24 +57,33 @@ public class arraylist {
     }
 
     static void main() {
-        arraylist l = new arraylist();
-        l.add(1);
-        l.add(2);
-        l.add(4);
-        l.add(3);
-        l.add(5);
-        l.add(6);
-        System.out.println(l.remove());
-        System.out.println(l.remove());
-System.out.println(l.Size());
-l.add(100);
-System.out.println(l.get());
-l.set(1,1000);
-System.out.println(l.get(1));
-System.out.println("------------------------------------------------------------------");
-        for(int i = 0 ; i<l.Size() ; i++) {
-            System.out.println(l.get(i));
+
+        arraylist<String> obj = new arraylist<>();
+
+        for(int i = 0 ; i < 12 ; i++){
+            char a = (char) (i+'a');
+            obj.add(a);
         }
+System.out.print(obj);
+
+//        arraylist l = new arraylist();
+//        l.add(1);
+//        l.add(2);
+//        l.add(4);
+//        l.add(3);
+//        l.add(5);
+//        l.add(6);
+//        System.out.println(l.remove());
+//        System.out.println(l.remove());
+//System.out.println(l.Size());
+//l.add(100);
+//System.out.println(l.get());
+//l.set(1,1000);
+//System.out.println(l.get(1));
+//System.out.println("------------------------------------------------------------------");
+//        for(int i = 0 ; i<l.Size() ; i++) {
+//            System.out.println(l.get(i));
+//        }
 //        System.out.println(l.get());
 //        for(int i = 0 ; i <17 ; i++){
 //            l.add(i);
